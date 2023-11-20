7b7e57d2a010260e6dbdc8ec58430ee9293ce8ea
TPShipilova <138506392+TPShipilova@users.noreply.github.com>
2023-11-20 18:09:07 +0300

    Update test.cpp

--- 2/test.cpp
+++ 2/test.cpp
@@ -1,125 +1,149 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include "binary.h"
-#include "gtest/gtest.h"
-#include <stdexcept>
 
-TEST(BinaryOperatorPlus, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    Binary result = b1 + b2;
-    ASSERT_EQ(result.getBits(), "101010");
+TEST(lessGreatEq, test_01)
+{
+    Binary bin1("100"), bin("100");
+    ASSERT_TRUE( bin1 == bin );
 }
 
-TEST(BinaryOperatorPlus, DifferentBitValues){
-    Binary b1("10101");
-    Binary b2("11011");
-    Binary result = b1 + b2;
-    ASSERT_EQ(result.getBits(), "110000");
+TEST(lessGreatEq, test_00)
+{
+    Binary bin1, bin;
+    ASSERT_TRUE( bin1 == bin );
 }
 
-TEST(BinaryOperatorMinus, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    Binary result = b1 - b2;
-    ASSERT_EQ(result.getBits(), "00000");
+TEST(lessGreatEq, test_000)
+{
+    Binary bin1("10"), bin;
+    ASSERT_FALSE( bin1 == bin );
 }
 
-TEST(test_02, subtract_from_smaller_number){  
-    Binary b1("1010"); 
-    Binary b2("1111"); 
-    ASSERT_THROW(b2-b1, std::invalid_argument); 
+TEST(lessGreatEq, test_02)
+{
+    Binary bin1("10"), bin("101");
+    ASSERT_FALSE( bin1 == bin );
 }
 
-TEST(BinaryOperatorEquals, AssignToAnotherBinary){
-    Binary b1("10101");
-    Binary b2("11011");
-    b1 = b2;
-    ASSERT_EQ(b1.getBits(), "11011");
+TEST(lessGreatEq, test_03)
+{
+    Binary bin1("100"), bin("10");
+    ASSERT_TRUE( bin1 > bin );
 }
 
-TEST(BinaryOperatorEquals, AssignToItself){
-    Binary b1("10101");
-    b1 = b1;
-    ASSERT_EQ(b1.getBits(), "10101");
+TEST(lessGreatEq, test_04)
+{
+    Binary bin1("0"), bin = {'1','0','0'};
+    ASSERT_TRUE( bin1 < bin );
 }
 
-TEST(BinaryOperatorEquals, AssignEmptyBinary){
-    Binary b1("10101");
-    Binary b2;
-    b1 = b2;
-    ASSERT_EQ(b1.getBits(), "");
+TEST(lessGreatEq, test_05)
+{
+    Binary bin1(4, '1'), bin("1111");
+    ASSERT_TRUE( bin1 == bin );
 }
 
-TEST(BinaryOperatorEquals, AssignToEmptyBinary){
-    Binary b1;
-    Binary b2("11011");
-    b1 = b2;
-    ASSERT_EQ(b1.getBits(), "11011");
+TEST(lessGreatEq, test_06)
+{
+    Binary bin("100");
+    Binary bin1;
+    bin1 = bin;
+    ASSERT_TRUE( bin1 == bin );
 }
 
-TEST(BinaryOperatorEquals, AssignEmptyBinaryToEmptyBinary){
-    Binary b1;
-    Binary b2;
-    b1 = b2;
-    ASSERT_EQ(b1.getBits(), "");
+TEST(lessGreatEq, test_07)
+{
+    Binary bin(3, '1');
+    Binary bin1;
+    bin1 = bin;
+    ASSERT_TRUE( bin1 == bin );
 }
 
-TEST(BinaryOperatorEqual, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    ASSERT_TRUE(b1 == b2);
+TEST(lessGreatEq, test_08)
+{
+    Binary bin(3, '1');
+    Binary binh(3, '1');
+    Binary bin1;
+    Binary t;
+    bin1 = std::move(bin);
+    ASSERT_TRUE( bin1 == binh );
+    ASSERT_TRUE( bin == t );
 }
 
-TEST(BinaryOperatorEqual, DifferentBitValues){
-    Binary b1("10101");
-    Binary b2("11011");
-    ASSERT_FALSE(b1 == b2);
+TEST(PlusMinus, test_09)
+{
+    Binary bin1("0"), bin("100");
+    Binary bins;
+    bins = bin1 + bin;
+    ASSERT_TRUE( bin1 + bin == bins );
 }
 
-TEST(BinaryOperatorNotEqual, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    ASSERT_FALSE(b1 != b2);
+TEST(PlusMinus, test_10)
+{
+    Binary bin1("10"), bin("100");
+    Binary bins("110");
+    ASSERT_TRUE( bin1 + bin == bins );
 }
 
-TEST(BinaryOperatorNotEqual, DifferentBitValues){
-    Binary b1("10101");
-    Binary b2("11011");
-    ASSERT_TRUE(b1 != b2);
+TEST(PlusMinus, test_11)
+{
+    Binary bin1("110"), bin("11");
+    Binary bins("1001");
+    ASSERT_TRUE( bin1 + bin == bins );
 }
 
-TEST(BinaryOperatorLessThan, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    ASSERT_FALSE(b1 < b2);
+TEST(PlusMinus, test_12)
+{
+    Binary bin1(4, '1'), bin("100");
+    Binary bins("10011");
+    ASSERT_TRUE( bin1 + bin == bins );
 }
 
-TEST(BinaryOperatorLessThan, FirstBinarySmaller){
-    Binary b1("10101");
-    Binary b2("11011");
-    ASSERT_TRUE(b1 < b2);
+TEST(PlusMinus, test_13)
+{
+    Binary bin1("100"), bin("0");
+    Binary bins;
+    bins = bin1 - bin;
+    ASSERT_TRUE( bin1 - bin == bins );
 }
 
-TEST(BinaryOperatorLessThan, FirstBinaryLarger){
-    Binary b1("11011");
-    Binary b2("10101");
-    ASSERT_FALSE(b1 < b2);
+TEST(PlusMinus, test_14)
+{
+    Binary bin1("110"), bin("10");
+    Binary bins("100");
+    ASSERT_TRUE( bin1 - bin == bins );
 }
 
-TEST(BinaryOperatorGreaterThan, SameBitValues){
-    Binary b1("10101");
-    Binary b2("10101");
-    ASSERT_FALSE(b1 > b2);
+TEST(PlusMinus, test_15)
+{
+    Binary bin1("0"), bin("0");
+    Binary bins("0");
+    ASSERT_TRUE( bin1 - bin == bins );
 }
 
-TEST(BinaryOperatorGreaterThan, FirstBinarySmaller){
-    Binary b1("10101");
-    Binary b2("11011");
-    ASSERT_FALSE(b1 > b2);
+TEST(PlusMinus, test_16)
+{
+    Binary bin1(4,'1'), bin("1011");
+    Binary bins("100");
+    ASSERT_TRUE( bin1 - bin == bins );
 }
 
-TEST(BinaryOperatorGreaterThan, FirstBinaryLarger){
-    Binary b1("11011");
-    Binary b2("10101");
-    ASSERT_TRUE(b1 > b2);
+TEST(PlusMinus, test_17)
+{
+    Binary bin1(4,'1'), bin;
+    Binary bins("1111");
+    ASSERT_TRUE( bin1 - bin == bins );
+}
+
+TEST(PlusMinus, test_18)
+{
+    Binary bin1(4,'1'), bin;
+    Binary bins("1111");
+    ASSERT_TRUE( bin1 + bin == bins );
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
