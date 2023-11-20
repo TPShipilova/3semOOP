8e56d05d18ceb5b3123b8b7e83484a634e71cd3a
TPShipilova <138506392+TPShipilova@users.noreply.github.com>
2023-11-20 18:12:08 +0300

    Update figure.hpp

--- 3/figure.hpp
+++ 3/figure.hpp
@@ -9,7 +9,7 @@ class Figure{
     public:
         Figure() = default; 
         virtual double square() const = 0;
-        virtual Point center() const = 0; //????
-        virtual operator double() const = 0; //???????
+        virtual Point center() const = 0; 
+        virtual operator double() const = 0; 
         ~Figure() = default; 
 };
