7fa711e7945b6495dea16b7ab062a4ebdfcb8c04
TPShipilova <138506392+TPShipilova@users.noreply.github.com>
2023-11-20 18:07:44 +0300

    Update binary.h

--- 2/binary.h
+++ 2/binary.h
@@ -1,24 +1,34 @@
 #pragma once
 
+#include <string>
 #include <iostream>
-#include <stdexcept>
+#include <ostream>
+#include <initializer_list>
 
 class Binary {
-private:
-    unsigned char* data;
-    int length;
 public:
-    Binary(int len);
-    Binary(const Binary& other);
-    ~Binary();
-    void setBit(int pos, bool value);
-    bool getBit(int pos) const;
-    Binary operator+(const Binary& other) const;
-    Binary operator-(const Binary& other) const;
-    Binary& operator=(const Binary& other);
-    bool operator==(const Binary& other) const;
-    bool operator!=(const Binary& other) const;
-    bool operator<(const Binary& other) const;
-    bool operator>(const Binary& other) const;
-    
+    Binary();
+    Binary (const std::initializer_list<unsigned char> & t);
+    Binary (const std::string & t);
+    Binary (const Binary &other);
+    Binary (Binary &&other) noexcept;
+    Binary(const size_t &n, unsigned char t);
+
+    Binary& operator = (const Binary& other);
+    Binary& operator = (Binary&& other) noexcept;
+
+    bool operator == (const Binary & t) const;
+    bool operator > (const Binary &other) const;
+    bool operator < (const Binary &other) const;
+    Binary operator + (const Binary & t) const;
+    Binary operator - (const Binary & t) const;
+
+    virtual ~Binary() noexcept;
+
+    friend std::ostream& operator << (std::ostream& os, const Binary& number);
+
+private:
+    size_t _size;
+    unsigned char* _array;
+    void copyDigits(const unsigned char* otherDigits, size_t otherSize);
 };
